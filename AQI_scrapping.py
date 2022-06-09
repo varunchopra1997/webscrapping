9e0a1a80a89eca00366a0fbb5203b36480d2a06c
varunchopra1997 <56476562+varunchopra1997@users.noreply.github.com>
2022-06-09 13:34:38 +0530

    Update AQI_scrapping.py

--- AQI_scrapping.py
+++ AQI_scrapping.py
@@ -1,7 +1,8 @@
 import requests
 import pandas as pd
-
-def item_flat(item):
+import datetime
+import time
+def item_flat(item,batch_time):
     record=[]
     data=[]
     data.append(item['city']['name'])
@@ -37,6 +38,7 @@ def item_flat(item):
         data.append(item['time']['s'])
     except:
         data.append('None')
+    data.append(batch_time)
 
 
     record+=[data]
@@ -52,26 +54,31 @@ endpoint_url='https://api.waqi.info/feed/delhi/?token='
 endpoint_url+= api_key
 
 
-usa=['Montgomery', 'Phoenix', 'Little Rock', 'Sacramento', 'Denver', 'Hartford', 'Tallahassee', 'Atlanta', 'Honolulu', 'Boise', 'Springfield', 'Indianapolis', 'Des Moines', 'Topeka', 'Frankfort', 'Baton Rouge', 'Augusta', 'Boston', 'Lansing', 'Saint Paul', 'Jackson', 'Jefferson City', 'Helena', 'Lincoln', 'Concord', 'Trenton', 'Albany', 'Raleigh', 'Bismarck', 'Columbus', 'Oklahoma City', 'Salem', 'Harrisburg', 'Providence', 'Columbia', 'Nashville', 'Austin', 'Salt Lake City', 'Richmond', 'Olympia', 'Charleston', 'Madison', 'Cheyenne']
+usa=['Montgomery', 'Phoenix', 'Little Rock', 'Sacramento', 'Denver', 'Hartford', 'Tallahassee', 'Atlanta', 'Honolulu', 'Boise', 'Springfield', 'Indianapolis', 'Des Moines', 'Topeka', 'Baton Rouge', 'Augusta', 'Boston', 'Lansing', 'Saint Paul', 'Jackson', 'Jefferson City', 'Helena', 'Lincoln', 'Concord', 'Trenton', 'Albany', 'Raleigh', 'Bismarck', 'Columbus', 'Oklahoma City', 'Salem', 'Harrisburg', 'Providence', 'Columbia', 'Nashville', 'Austin', 'Salt Lake City', 'Richmond', 'Olympia', 'Charleston', 'Madison', 'Cheyenne','delhi','gurgaon','mumbai','chennai','bangalore','gandhinagar','patna','kolkata','chandigarh','bhopal','Thiruvananthapuram','jaipur']
+
+
+cities= ['delhi','gurgaon','mumbai','chennai','bangalore','gandhinagar','patna','kolkata','chandigarh','bhopal','Thiruvananthapuram','jaipur']
 
 
 for city in usa:
     api_key='95cdbb0758cb209e7485dd3daf69fae76f71fc7e'
     endpoint_url='https://api.waqi.info/feed/'+city+'/?token='
     endpoint_url+= api_key
-    response= requests.get(endpoint_url)
+    #response= requests.get(endpoint_url)
     received_data= requests.get(endpoint_url).json()
     data.append(received_data['data'])
 
+batch_time= str(datetime.datetime.now())[0:19]
 
 output=[]
 for item in data:
-    output+=item_flat(item)
+    output+=item_flat(item,batch_time)
 
 
-headers= ['station','aqi','latitude','longitude','co','no2','o3','so2','pm10','pm2.5','time']
+headers= ['station','aqi','latitude','longitude','co','no2','o3','so2','pm10','pm2.5','time','batch_time']
 
 df= pd.DataFrame(output,columns=headers)
 
 df.to_csv('aqi_scrapping_data.csv',header=True, index=False)
 
+datetime.datetime.now()
